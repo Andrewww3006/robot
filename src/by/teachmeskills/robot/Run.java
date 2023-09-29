0bba580212eb72579260bb9976090a26988206de
Andrew <andrew_3006@mail.ru>
2023-09-29 10:49:13 +0300

    my first commit

--- src/by/teachmeskills/robot/Run.java
+++ src/by/teachmeskills/robot/Run.java
@@ -5,9 +5,11 @@ import by.teachmeskills.robot.hands.SonyHands;
 import by.teachmeskills.robot.hands.ToshibaHands;
 import by.teachmeskills.robot.heads.IHead;
 import by.teachmeskills.robot.heads.SonyHead;
+import by.teachmeskills.robot.heads.ToshibaHead;
 import by.teachmeskills.robot.legs.ILeg;
 import by.teachmeskills.robot.legs.SamsungLegs;
 import by.teachmeskills.robot.legs.SonyLegs;
+import by.teachmeskills.robot.legs.ToshibaLegs;
 
 public class Run {
     public static void main(String[] args) {
@@ -23,12 +25,12 @@ public class Run {
         IHand sonyHand = new SonyHands(1000);
         ILeg  sonyLeg = new SonyLegs(700);
 
-        IHead toshibaHead = new SonyHead(15000);
-        IHand sonyHand = new SonyHands(1000);
-        ILeg  sonyLeg = new SonyLegs(700);
+        IHead toshibaHead = new ToshibaHead(15000);
+        IHand toshibaHand = new ToshibaHands(1000);
+        ILeg  toshibaLeg = new ToshibaLegs(700);
 
 
-        Robot robot1 = new Robot(sonyHead, sonyHand, );
+        Robot robot1 = new Robot(sonyHead, sonyHand, sonyLeg);
 
 
     }
